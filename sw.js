1f0518a589d579df99032e220b97215e9a8d6818
Coyld1605 <fsnwei@gmail.com>
2026-09-06 17:21:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "invest-v2";
+const CACHE = "invest-v3";
 self.addEventListener("install", (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./manifest.json"])));
